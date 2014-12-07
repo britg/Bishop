6e47979281eb0597f1dd77788b6aaaa4560a7755
britg <brit@britg.com>
2014-12-07 14:57:43 -0600

    Initial commit

---
+++ Assets/Scripts/Controllers/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : GameController {
+
+	float consideredEqual = 0.001f;
+
+	public Player player;
+	public Player.Properties playerInitialization;
+
+	Player.Direction nextDirection = Player.Direction.Up;
+	bool nextWaypointSet = false;
+	Vector3 nextWaypoint = Vector3.zero;
+
+	void Awake () {
+		player = new Player(playerInitialization);
+	}
+
+	void Update () {
+		HandleDirectionChange();
+		MovePlayer();
+	}
+
+	void MovePlayer () {
+		ChooseNextWaypoint();
+		transform.position += player.Velocity * Time.deltaTime;
+	}
+
+	void HandleDirectionChange () {
+		if (Input.GetKeyDown(KeyCode.W)) {
+			nextDirection = Player.Direction.Up;
+		}
+		if (Input.GetKeyDown(KeyCode.A)) {
+			nextDirection = Player.Direction.Left;
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			nextDirection = Player.Direction.Down;
+		}
+		if (Input.GetKeyDown(KeyCode.D)) {
+			nextDirection = Player.Direction.Right;
+		}
+	}
+
+	void ChooseNextWaypoint () {
+		if (!nextWaypointSet) {
+			nextWaypoint = transform.position + player.DirectionVector;
+			nextWaypointSet = true;
+		}
+
+		if (AtWaypoint()) {
+			if (player.CurrentDirection != nextDirection) {
+				Debug.Log ("Changing player direction at " + transform.position);
+			}
+			player.CurrentDirection = nextDirection;
+			nextWaypoint = transform.position + player.DirectionVector;
+		}
+	}
+
+	bool AtWaypoint () {
+		return (Vector3.SqrMagnitude(transform.position - nextWaypoint) < consideredEqual);
+	}
+}
---
+++ Assets/Scripts/Models/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : GameModel {
+
+	[System.Serializable]
+	public class Properties {
+		public float speed;
+	}
+
+	public enum Direction {
+		Up,
+		Down,
+		Right,
+		Left
+	}
+
+	public float Speed { get; set; }
+	public Direction CurrentDirection { get; set; }
+
+	public Vector3 DirectionVector {
+		get {
+			if (CurrentDirection == Player.Direction.Up) {
+				return Vector3.forward;
+			}
+			if (CurrentDirection == Player.Direction.Right) {
+				return Vector3.right;
+			}
+			if (CurrentDirection == Player.Direction.Left) {
+				return Vector3.left;
+			}
+			if (CurrentDirection == Player.Direction.Down) {
+				return Vector3.back;
+			}
+
+			return Vector3.forward;
+		}
+	}
+
+	public Vector3 Velocity {
+		get {
+			return Speed * DirectionVector;
+		}
+	}
+
+	public Player (Properties props) {
+		Speed = props.speed;
+		CurrentDirection = Direction.Up;
+	}
+
+}
