3306d0179f16e1116c191a757dfae0eaa7c851bb
britg <brit@britg.com>
2014-12-14 07:49:30 -0600

    Snap to waypoints when close enough

--- Assets/Scripts/Controllers/Player/PlayerController.cs
+++ Assets/Scripts/Controllers/Player/PlayerController.cs
@@ -11,8 +11,22 @@ public class PlayerController : GameController {
 	Player.Direction nextDirection;
 	bool nextWaypointSet = false;
 	Vector3 nextWaypoint = Vector3.zero;
+	float currentMoveDistance = 0f;
+	float waypointDistance = 1f;
 
 	bool canMove = false;
+
+	Vector3 frameSwipeDistance = Vector3.zero;
+	Vector3 currentSwipeDistance = Vector3.zero;
+	float requiredSwipeDistance = 100f;
+	enum SwipeDirection {
+		None,
+		Up,
+		Down,
+		Right,
+		Left
+	}
+	SwipeDirection swipeDirection = SwipeDirection.None;
 
 	void Awake () {
 		player = new Player(playerInitialization);
@@ -22,6 +36,7 @@ public class PlayerController : GameController {
 
 	void Update () {
 		CheckDead();
+		AccumulateSwipeDuration();
 		HandleDirectionChange();
 		if (canMove) {
 			MovePlayer();
@@ -29,8 +44,21 @@ public class PlayerController : GameController {
 	}
 
 	void MovePlayer () {
+		Vector3 frameMove = player.Velocity * Time.deltaTime;
+		float frameMagnitude = player.Speed * Time.deltaTime;
+		float nextMoveDistance = currentMoveDistance + frameMagnitude;
+		if (nextMoveDistance > waypointDistance) {
+			float remaining = waypointDistance - currentMoveDistance;
+			float pct = remaining / waypointDistance;
+			currentMoveDistance = waypointDistance;
+			transform.position = nextWaypoint;
+		} else {
+			currentMoveDistance += frameMagnitude;
+			transform.position += frameMove;
+		}
+//		currentMoveDistance += player.Speed * Time.deltaTime;
+
 		ChooseNextWaypoint();
-		transform.position += player.Velocity * Time.deltaTime;
 	}
 
 	void EnableMovement () {
@@ -42,18 +70,115 @@ public class PlayerController : GameController {
 	}
 
 	void HandleDirectionChange () {
-		if (Input.GetKeyDown(KeyCode.W)) {
+		TouchDirection();
+		if (HandleUp()) {
 			nextDirection = Player.Direction.Up;
+			Debug.Log ("Up");
+			return;
 		}
-		if (Input.GetKeyDown(KeyCode.A)) {
-			nextDirection = Player.Direction.Left;
-		}
-		if (Input.GetKeyDown(KeyCode.S)) {
+		if (HandleDown()) {
 			nextDirection = Player.Direction.Down;
+			Debug.Log ("Down");
+			return;
 		}
-		if (Input.GetKeyDown(KeyCode.D)) {
+		if (HandleLeft()) {
+			nextDirection = Player.Direction.Left;
+			Debug.Log ("Left");
+			return;
+		}
+		if (HandleRight()) {
 			nextDirection = Player.Direction.Right;
+			Debug.Log ("Right");
+			return;
 		}
+	}
+
+	Vector3 TouchDirection () {
+		Vector3 direction = Vector3.zero;
+		if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Moved) {
+			direction = Input.touches[0].deltaPosition;
+		}
+		return direction;
+	}
+
+	void AccumulateSwipeDuration () {
+
+		if (Input.touchCount < 1) {
+			currentSwipeDistance = Vector3.zero;
+			swipeDirection = SwipeDirection.None;
+			return;
+		}
+
+		frameSwipeDistance = TouchDirection();
+		currentSwipeDistance += frameSwipeDistance;
+
+		if (currentSwipeDistance.y >= requiredSwipeDistance) {
+			swipeDirection = SwipeDirection.Up;
+			return;
+		}
+
+		if (currentSwipeDistance.x >= requiredSwipeDistance) {
+			swipeDirection = SwipeDirection.Right;
+			return;
+		}
+
+		if (currentSwipeDistance.y <= -requiredSwipeDistance) {
+			swipeDirection = SwipeDirection.Down;
+			return;
+		}
+
+		if (currentSwipeDistance.x <= -requiredSwipeDistance) {
+			swipeDirection = SwipeDirection.Left;
+			return;
+		}
+	}
+
+	bool HandleUp () {
+		if (Input.GetKeyDown(KeyCode.W)) {
+			return true;
+		}
+
+		if (swipeDirection == SwipeDirection.Up) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool HandleDown () {
+		if (Input.GetKeyDown(KeyCode.S)) {
+			return true;
+		}
+
+		if (swipeDirection == SwipeDirection.Down) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	bool HandleRight () {
+		if (Input.GetKeyDown(KeyCode.D)) {
+			return true;
+		}
+
+		if (swipeDirection == SwipeDirection.Right) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	bool HandleLeft () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			return true;
+		}
+
+		if (swipeDirection == SwipeDirection.Left) {
+			return true;
+		}
+		
+		return false;
 	}
 
 	void ChooseNextWaypoint () {
@@ -63,6 +188,12 @@ public class PlayerController : GameController {
 		}
 
 		if (AtWaypoint()) {
+//			if (player.CurrentDirection != Player.Direction.Stop) {
+//				Debug.Log ("Current move distance " + currentMoveDistance);
+//				Debug.Log ("Transform " + transform.position);
+//				nextDirection = Player.Direction.Stop;
+//			}
+			currentMoveDistance = 0f;
 			player.CurrentDirection = nextDirection;
 			nextWaypoint = transform.position + player.DirectionVector;
 
@@ -74,7 +205,8 @@ public class PlayerController : GameController {
 
 	bool AtWaypoint () {
 		return player.CurrentDirection == Player.Direction.Stop ||
-			(Vector3.SqrMagnitude(transform.position - nextWaypoint) < consideredEqual);
+			(Vector3.SqrMagnitude(transform.position - nextWaypoint) < consideredEqual) ||
+				currentMoveDistance >= waypointDistance;
 	}
 
 	bool WaypointValid (Vector3 waypoint) {
