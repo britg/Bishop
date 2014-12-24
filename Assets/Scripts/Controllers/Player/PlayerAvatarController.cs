3ed9c4b28aeabda09e3407bbae6b231c18633c4a
britg <brit@britg.com>
2014-12-24 09:02:09 -0600

    Show sword when you have a sword

---
+++ Assets/Scripts/Controllers/Player/PlayerAvatarController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAvatarController : GameController {
+
+	Player player;
+	public GameObject sword;
+
+	// Use this for initialization
+	void Start () {
+		player = GetPlayer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShowHideSword();
+	}
+
+	void ShowHideSword () {
+		if (player.Swords > 0) {
+			sword.SetActive(true);
+		} else {
+			sword.SetActive(false);
+		}
+	}
+}
