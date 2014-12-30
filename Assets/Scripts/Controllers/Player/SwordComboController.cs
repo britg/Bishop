e6a0b8f7b1f4a7356f357fca6fb5c594f25b540e
britg <brit@britg.com>
2014-12-30 11:01:53 -0600

    Start the sword combo system

---
+++ Assets/Scripts/Controllers/Player/SwordComboController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordComboController : GameController {
+
+	Player player;
+
+	int COMBO_COUNT = 10;
+
+	// Use this for initialization
+	void Start () {
+		player = GetPlayer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.CurrentCombo >= COMBO_COUNT) { // temp
+			player.Swords++;
+			player.CurrentCombo = 0;
+		}
+	}
+}
