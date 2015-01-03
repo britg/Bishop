cdb92c6c3c91b1eaacd381d82f25a47d78e7efba
britg <brit@britg.com>
2015-01-02 19:23:14 -0600

    Start of new concept

--- Assets/Scripts/Controllers/Player/TouchInputController.cs
+++ Assets/Scripts/Controllers/Player/TouchInputController.cs
@@ -18,6 +18,17 @@ public class TouchInputController : GameController {
 	Vector3 currentSwipeDistance = Vector3.zero;
 	public float requiredSwipeDistance = 100f;
 
+	Vector3 lastMousePosition;
+
+	int touchCount {
+		get {
+			if (Input.GetMouseButton(0)) {
+				return 1;
+			}
+			return Input.touchCount;
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
 		player = GetPlayer();
@@ -25,6 +36,9 @@ public class TouchInputController : GameController {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			lastMousePosition = Input.mousePosition;
+		}
 		AccumulateSwipeDuration();
 		HandleDirectionChange();
 	}
@@ -53,39 +67,47 @@ public class TouchInputController : GameController {
 		Vector3 direction = Vector3.zero;
 		if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Moved) {
 			direction = Input.touches[0].deltaPosition;
+		} else if (Input.GetMouseButton(0)) {
+			direction = Input.mousePosition - lastMousePosition;
+			lastMousePosition = Input.mousePosition;
 		}
 		return direction;
 	}
 	
 	void AccumulateSwipeDuration () {
+
+		frameSwipeDistance = TouchDirection();
 		
-		if (Input.touchCount < 1) {
+		if (frameSwipeDistance.Equals(Vector3.zero)) {
 			currentSwipeDistance = Vector3.zero;
 			swipeDirection = SwipeDirection.None;
 			return;
 		}
 		
-		frameSwipeDistance = TouchDirection();
 		currentSwipeDistance += frameSwipeDistance;
-		
-		if (currentSwipeDistance.y >= requiredSwipeDistance) {
-			swipeDirection = SwipeDirection.Up;
-			return;
-		}
-		
-		if (currentSwipeDistance.x >= requiredSwipeDistance) {
-			swipeDirection = SwipeDirection.Right;
-			return;
-		}
-		
-		if (currentSwipeDistance.y <= -requiredSwipeDistance) {
-			swipeDirection = SwipeDirection.Down;
-			return;
-		}
-		
-		if (currentSwipeDistance.x <= -requiredSwipeDistance) {
-			swipeDirection = SwipeDirection.Left;
-			return;
+
+		if (Mathf.Abs(currentSwipeDistance.y) > Mathf.Abs(currentSwipeDistance.x)) {
+
+			if (currentSwipeDistance.y >= requiredSwipeDistance) {
+				swipeDirection = SwipeDirection.Up;
+				return;
+			}
+
+			if (currentSwipeDistance.y <= -requiredSwipeDistance) {
+				swipeDirection = SwipeDirection.Down;
+				return;
+			}
+		} else {
+
+			if (currentSwipeDistance.x >= requiredSwipeDistance) {
+				swipeDirection = SwipeDirection.Right;
+				return;
+			}
+
+			if (currentSwipeDistance.x <= -requiredSwipeDistance) {
+				swipeDirection = SwipeDirection.Left;
+				return;
+			}
 		}
 	}
 	
